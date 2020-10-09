b9c7a9ca7a858099198070668b36b117f184ea8d
Alexander Balashov <alaex777@gmail.com>
2020-10-09 18:03:06 +0300

    Changed classifier.py

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import re as re
-s = "fgbadbgjkerjg3478t54fjk$?kgekleg"
-print(re.split('[^0-9a-z]', s))
+from typing import List, Any
+
+def predassembling( train_texts: List[str]) -> Any:
+    result = list()
+    for i in range(len(train_texts)):
+        tmp = re.split('[^0-9a-z]', train_texts[i].lower())
+        tmp = [j for j in tmp if j]
+        result.append(tmp)
+    return result
+
+inp = ["Very good, thank you!!", "Awful, i'm kidding, lol:)", "As     fddhfg99 said, shiiiit", "Niiice))))"]
+print(predassembling(inp))
