b4345cf84cba8f64705c920b55e7e4ee91eab90a
Alexander Balashov <alaex777@gmail.com>
2020-10-14 11:53:00 +0400

    First try, accuracy 65%

---
+++ classifier.py
@@ -0,0 +1,138 @@
+from typing import List, Set, Dict, Any
+from random import random
+import re as re
+import math as math
+
+
+def count_labels(labels: List):
+    return {
+        unique_label: sum(1 for label in labels if label == unique_label)
+        for unique_label in set(labels)
+    }
+
+def predassembling( train_texts: List[str]) -> Any:
+    result = list()
+    for i in range(len(train_texts)):
+        tmp = re.split('[^0-9a-z]', train_texts[i].lower())
+        tmp = [j for j in tmp if len(j) > 3]
+        result.append(tmp)
+    return result
+
+def make_dict(set1: Set, list1: List) -> Dict:
+    params = dict()
+    tmpp = set()
+    for i in list1:
+        tmp = set()
+        for j in i:
+            tmp.add(j)
+        for j in tmp:
+            if j in tmpp:
+                params[j] += 1/len(list1)
+            else:
+                tmpp.add(j)
+                params[j] = 1/len(list1)
+    return params
+
+def train(
+        train_texts: List[str],
+        train_labels: List[str],
+        pretrain_params: Any = None) -> Any:
+    """
+    Trains classifier on the given train set represented as parallel lists of texts and corresponding labels.
+    :param train_texts: a list of texts (str objects), one str per example
+    :param train_labels: a list of labels, one label per example
+    :param pretrain_params: parameters that were learned at the pretrain step
+    :return: learnt parameters, or any object you like (it will be passed to the classify function)
+    """
+    positive = list()
+    negative = list()
+    pos_set = set()
+    neg_set = set()
+    pos_params = dict()
+    neg_params = dict()
+    pos_length = 0
+    neg_length = 0
+    predlist = predassembling(train_texts)
+    
+    for i in range(len(predlist)):
+        if train_labels[i] == "pos":
+            pos_length += 1
+            positive.append(predlist[i])
+            for j in predlist[i]:
+                pos_set.add(j)
+        elif train_labels[i] == "neg":
+            neg_length += 1
+            negative.append(predlist[i])
+            for j in predlist[i]:
+                neg_set.add(j)
+
+    pos_params = make_dict(pos_set, positive)
+    neg_params = make_dict(neg_set, negative)
+
+    res = [pos_set, neg_set, pos_params, neg_params, pos_length, neg_length]
+
+    return res
+
+    # ############################ REPLACE THIS WITH YOUR CODE #############################
+    #label2cnt = count_labels(train_labels)  # count labels
+    #print('Labels counts:', label2cnt)
+    #train_size = sum(label2cnt.values())
+    #label2prob = {label: cnt / train_size for label, cnt in label2cnt.items()}  # calculate p(label)
+    #print(label2prob)
+    #return {'prior': label2prob}  # this dummy classifier learns prior probabilities of labels p(label)
+    # ############################ REPLACE THIS WITH YOUR CODE #############################
+
+
+def pretrain(texts_list: List[List[str]]) -> Any:
+    """
+    Pretrain classifier on unlabeled texts. If your classifier cannot train on unlabeled data, skip this.
+    :param texts_list: a list of list of texts (str objects), one str per example.
+        It might be several sets of texts, for example, train and unlabeled sets.
+    :return: learnt parameters, or any object you like (it will be passed to the train function)
+    """
+    # ############################ PUT YOUR CODE HERE #######################################
+    return None
+
+
+def classify(texts: List[str], params: Any) -> List[str]:
+    """
+    Classify texts given previously learnt parameters.
+    :param texts: texts to classify
+    :param params: parameters received from train function
+    :return: list of labels corresponding to the given list of texts
+    """
+
+    pos_set = params[0]
+    neg_set = params[1]
+    pos_params = params[2]
+    neg_params = params[3]
+    pos_length = params[4]
+    neg_length = params[5]
+
+    inp = predassembling(texts)
+
+    res = list()
+
+    for i in inp:
+        pos_p = sum(math.log(pos_params[j]) for j in i if j in pos_set) * pos_length / (pos_length + neg_length)
+        neg_p = sum(math.log(neg_params[j]) for j in i if j in neg_set) * neg_length / (pos_length + neg_length)
+        if pos_p > neg_p:
+            res.append("pos")
+        else:
+            res.append("neg")
+    return res
+       
+    # ############################ REPLACE THIS WITH YOUR CODE #############################
+    #def random_label(_label2prob):
+    #    rand = random()  # random value in [0.0, 1.0) from uniform distribution
+    #    for label, prob in _label2prob.items():
+    #        rand -= prob
+    #        if rand <= 0:
+    #            return label
+
+    #label2prob = params['prior']
+    #res = [random_label(label2prob) for _ in texts]  # this dummy classifier returns random labels from p(label)
+    #print('Predicted labels counts:')
+    #print(count_labels(res))
+    #return res
+    # ############################ REPLACE THIS WITH YOUR CODE #############################
